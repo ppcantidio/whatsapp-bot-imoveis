dfa6f64f5527ffb8c4f48c1db02a06ac45dea67e
Pedro Cantidio <ppcantidio@gmail.com>
2022-09-25 16:51:18 -0300

    go horse

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 
 from flask import request
-from src.services.whatsapp_service import Whatsapp
+from clients.imoveis_natal.service import ImoveisNatal
 from src.services.webhook_service import Webhook
 
 
@@ -20,12 +20,20 @@ def imoveis_natal():
         return param
 
     response = request.get_json()
-    print(response)
-
     if Webhook().verifica_tipo_webhook(response):
-        Whatsapp().menssagem_simples("5584981631800", "Qual seu nome?")
+        message = Webhook().get_message(response)
+        number = message.get("number")
+        msg = message.get("message")
 
-    return response
+        step = Webhook().verify_step(number)
+        print(f"step: {step}")
+
+        if step == None and Webhook().verifica_cliente(response) is False:
+            ImoveisNatal().step_1_question(number)
+        elif step == 1:
+            ImoveisNatal().step_1_answer(msg, number)
+            ImoveisNatal().step_2_question(number)
+    return jsonify({"sucesso": True})
 
 
 if __name__ == "__main__":
